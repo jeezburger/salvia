6ef65618fa0f0d5fea27f2c48c8256cbfdb01564
vee <apple@vance.local>
2024-10-12 11:30:42 +0530

    updates

--- components/product.jsx
+++ components/product.jsx
@@ -2,11 +2,11 @@ import { GlareCard } from "./ui/glare-card";
 
 const GlareCardDemo = () => {
     return (
-        <div className="flex items-center justify-center h-screen" style={{ backgroundColor: '#1a1a1a' }}>
+        <div className="flex items-center justify-center h-screen bg-black">
             <GlareCard className="flex flex-col items-center justify-center">
                 <img
                     className="h-full w-full absolute inset-0 object-cover"
-                    src="/prod.jpeg"
+                    src="/card.png"
                     alt="Product" />
             </GlareCard>
         </div>
--- pages/product.js
+++ pages/product.js
@@ -1,13 +1,18 @@
-// Blog.jsx
 import React from 'react';
+import Head from 'next/head';
 import '/app/globals.css';
 import GlareCardDemo from '@/components/product';
+
 const Product = () => {
     return (
         <div>
+            <Head>
+                <title>Salvia</title>
+                <meta name="" content="" />
+            </Head>
 
             <GlareCardDemo />
-        </div >
+        </div>
     );
 };
 
